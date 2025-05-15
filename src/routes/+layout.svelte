1755919091ef15ed5875982e81c34aee8c8eca32
gabrielatwell1987 <gabrielatwell@proton.me>
2025-05-15 12:14:47 -0700

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,58 @@
+<script>
+	import '../app.css';
+
+	let navItems = $state([
+		{ href: '/', text: 'Home' },
+		{ href: '/posts', text: 'Blog Posts' },
+		{ href: '/new', text: 'New Post' }
+	]);
+	let { children } = $props();
+</script>
+
+<nav>
+	<ul>
+		{#each navItems as item}
+			<li>
+				<a href={item.href}>{item.text}</a>
+			</li>
+		{/each}
+	</ul>
+</nav>
+
+<main>
+	{@render children()}
+</main>
+
+<style>
+	nav {
+		padding: 1rem;
+		background-color: #f4f4f4;
+
+		& ul {
+			list-style: none;
+			padding: 0;
+			margin: 0;
+			display: flex;
+			justify-content: space-around;
+			gap: 1rem;
+
+			& a {
+				text-decoration: none;
+				color: #333;
+				font-family: var(--ancizar-semibold-font);
+				font-size: clamp(1rem, 2vw, 1.5rem);
+				font-weight: 600;
+
+				&:hover {
+					color: #666;
+				}
+			}
+		}
+	}
+
+	main {
+		padding: 2rem;
+		max-width: 800px;
+		margin: 0 auto;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,52 @@
+<script>
+	let title = $state('Think.Flow');
+	let description = $state('For streaming thoughts and ideas');
+	let tags = $state(['thoughts', 'ideas', 'streaming']);
+</script>
+
+<header>
+	<h1>{title}</h1>
+	<p>{description}</p>
+
+	<div class="tags">
+		{#each tags as tag}
+			<span class="tag">{tag}</span>
+		{/each}
+	</div>
+</header>
+
+<style>
+	header {
+		padding: 2rem;
+		text-align: center;
+
+		& h1 {
+			color: #333;
+			margin-bottom: 1rem;
+			font-family: var(--ancizar-black-font);
+			font-size: clamp(3rem, 6vw, 10em);
+			letter-spacing: 3px;
+		}
+
+		& p {
+			color: #666;
+			line-height: 1.6;
+			font-size: clamp(1rem, 3vw, 2rem);
+		}
+
+		& .tags {
+			display: flex;
+			gap: 1rem;
+			justify-content: center;
+			flex-wrap: wrap;
+			margin-top: -2em;
+
+			& .tag {
+				padding: 0.5rem 1rem;
+				border-radius: 10px;
+				font-size: clamp(0.8rem, 1vw, 1rem);
+				color: #555;
+			}
+		}
+	}
+</style>
